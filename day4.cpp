da0174b6e13163a8b28a8acf3b415c7c2b149589
Daria2002 <daria.matkovic@fer.hr>
2019-12-05 19:31:37 +0100

    Add solution for day4, part1.

--- day4.cpp
+++ day4.cpp
@@ -5,6 +5,10 @@
 #define LOWER 248345
 #define HIGHER 746315
 
+#define ASCII_ZERO 48
+
+int part1();
+
 /*
 It is a six-digit number.
 The value is within the range given in your puzzle input.
@@ -13,20 +17,43 @@ Going from left to right, the digits never decrease; they only ever increase or 
 */
 int main() {
 
+    std::cout << "part1 = " << part1() << std::endl;
+    
+
+}
+
+int part1() {
     int counter = 0;
     int tmp = 0;
     int previous = 0;
+    std::string num;
+    int digit;
+    int last_digit;
+    bool ascending;
+    bool duplicates_exists;
 
     for(int i = LOWER; i <= HIGHER; i++) {
+        ascending = true;
+        duplicates_exists = false;
+        num = std::to_string(i);
+        last_digit = num[0] - ASCII_ZERO;
 
-        for(int j = 1; j <= 6; j++) {
-            
-            tmp = pow(10, i);
-            if(previous < i/tmp) {
-
+        // check that digits never decrease
+        for(int j = 1; j < 6; j++, ascending == true) {
+            digit = num[j] - ASCII_ZERO;
+            if(digit < last_digit) {
+                ascending = false;
+                break;
             }
-            previous = i/tmp;
+            if(last_digit == digit) {
+                duplicates_exists = true;
+            }
+            last_digit = digit;
         }
-
+        if(ascending == true && duplicates_exists == true) {
+            counter++;
+        }
     }
+
+    return counter;
 }
