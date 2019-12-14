86da03f77b8c7fea387592aec1eeec648ee6147e
Daria2002 <daria.matkovic@fer.hr>
2019-12-14 13:29:31 +0100

    Add few things in day7, before adding header file

--- day7.cpp
+++ day7.cpp
@@ -1,8 +1,8 @@
 #include <iostream>
 #include <fstream>
 #include <sstream>
-
-
+#include <bits/stdc++.h>
+#include <algorithm>
 
 int main() {
 
@@ -11,7 +11,14 @@ int main() {
     std::string str;
     std::getline(file, str);
 
+    //  using int array
+    // int phases[] = {0, 1, 2, 3, 4};
+    // std::sort(phases, phases+5);
+
+    std::array<int, 5> phase_numbers = {0, 1, 2, 3, 4};
+    std::sort(phase_numbers.begin(), phase_numbers.end());
     
+    do {
 
-
+    } while (std::next_permutation(phase_numbers.begin(), phase_numbers.end()));
 }
