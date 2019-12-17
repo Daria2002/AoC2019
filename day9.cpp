2364332ba89703a607d74025d40a1ee28e591bc3
Daria2002 <daria.matkovic@fer.hr>
2019-12-17 20:53:07 +0100

    Add some code, but there is segmentation fault

--- day9.cpp
+++ day9.cpp
@@ -18,13 +18,13 @@
 class Intcode_calculator {
 
     enum Operations {
-        SUM = 1, 
-        MUL = 2, 
-        SAVE_INPUT = 3, 
-        OUTPUT = 4, 
+        SUM = 1,
+        MUL = 2,
+        SAVE_INPUT = 3,
+        OUTPUT = 4,
         JUMP_IF_TRUE = 5,
-        JUMP_IF_FALSE = 6, 
-        LESS_THAN = 7, 
+        JUMP_IF_FALSE = 6,
+        LESS_THAN = 7,
         EQUALS = 8,
         RELATIVE_BASE = 9,
         HALT = 99
@@ -35,168 +35,101 @@ class Intcode_calculator {
         IMMIDIATE  = 1,  
         RELATIVE = 2
     };
-
+   
     public:
-        Intcode_calculator(std::vector<int> _elements, int _input) :
-            elements(_elements), input(_input) 
+        std::map<int, std::function<void(std::array<long long int, 3>)>> functions;
+
+        Intcode_calculator(std::vector<long long int> _elements, int _input) : elements(_elements), input(_input)
         {
-            functions.emplace(1, [&]() {
+            functions.emplace(Operations::SUM, [&](std::array<long long int, 3> param) {
+                std::cout << "1" << std::endl;
                 num_of_params = 3;
-                std::array<long long int, 3> params;
-                for(int j = 0; j < num_of_params; j++) {
-                    if(indices_mode[j] == 0) {
-                        params[j] = elements[i+j+1];
-                    } else if(indices_mode[j] == 1) {
-                        params[j] = i+j+1;
-                    } else if(indices_mode[j] == 2) {
-                        params[j] = elements[i+j+1]+relative_base;
-                    }
-                }
-                elements[params[2]] = elements[params[0]] + elements[params[1]];
+                elements[param[2]] = elements[param[0]] + elements[param[1]];
             });
 
-            functions.emplace(2, [&]() {
+            functions.emplace(Operations::MUL, [&](std::array<long long int, 3> param) {
+                std::cout << "2" << std::endl;
                 num_of_params = 3;
-                std::array<long long int, 3> params;
-                for(int j = 0; j < num_of_params; j++) {
-                    if(indices_mode[j] == 0) {
-                        params[j] = elements[i+j+1];
-                    } else if(indices_mode[j] == 1) {
-                        params[j] = i+j+1;
-                    } else if(indices_mode[j] == 2) {
-                        params[j] = elements[i+j+1]+relative_base;
-                    }
-                }
-                elements[params[2]] = elements[params[0]] * elements[params[1]];    
+                elements[param[2]] = elements[param[0]] * elements[param[1]];    
             });
 
-            functions.emplace(3, [&]() {
+            functions.emplace(Operations::SAVE_INPUT, [&](std::array<long long int, 3> param) {
+                std::cout << "3" << std::endl;
                 num_of_params = 1;
-                int ind;
-                if(indices_mode[2] == 0) {
-                    ind = elements[i+1];
-                } else if(indices_mode[2] == 1) {
-                    ind = i+1;
-                } else if(indices_mode[2] == 2) {
-                    ind = elements[i+1]+relative_base;
-                }
-            
-                elements[ind] = input;
+               
+                //int ind;
+                //if(indices_mode[2] == 0) {
+                  //  ind = elements[i+1];
+                //} else if(indices_mode[2] == 1) {
+                  //  ind = i+1;
+                //} else if(indices_mode[2] == 2) {
+                 //   ind = elements[i+1]+relative_base;
+                //}
+                //elements[ind] = input;
+               
+                elements[param[0]] = input;
             });
 
-            functions.emplace(4, [&]() {
+            functions.emplace(Operations::OUTPUT, [&](std::array<long long int, 3> param) {
+                std::cout << "4" << std::endl;
                 num_of_params = 1;
-                if(indices_mode[0] == 0) {
-                    output = elements[elements[i+1]];
-                } else if(indices_mode[0] == 1) {
-                    output = elements[i+1];
-                } else if(indices_mode[0] == 2) {
-                    if(elements[i+1]+relative_base >= elements.size()) {
-                        // fill_with_zeros();
-                    }
-                    output = elements[elements[i+1]+relative_base];
-                }
+                output = elements[param[0]];
                 std::cout << "output = " << output << std::endl;
             });
 
-            functions.emplace(5, [&]() {
+            functions.emplace(Operations::JUMP_IF_TRUE, [&](std::array<long long int, 3> param) {
+                std::cout << "5" << std::endl;
                 num_of_params = 2;
-                std::array<long long int, 2> params;
-                for(int j = 0; j < num_of_params; j++) {
-                    if(indices_mode[j] == 0) {
-                        params[j] = elements[i+j+1];
-                    } else if(indices_mode[j] == 1) {
-                        params[j] = i+j+1;
-                    } else if(indices_mode[j] == 2) {
-                        params[j] = elements[i+j+1]+relative_base;
-                    }
-                }
-                if(elements[params[0]] != 0) {
+                if(elements[param[0]] != 0) {
                     // because there is num_of_params+1 in for loop
                     num_of_params = -1;
-                    i = elements[params[1]];
+                    i = elements[param[1]];
                 }
             });
 
-            functions.emplace(6, [&]() {
+            functions.emplace(Operations::JUMP_IF_FALSE, [&](std::array<long long int, 3> param) {
+                std::cout << "6" << std::endl;
                 num_of_params = 2;
-                std::array<long long int, 2> params;
-                for(int j = 0; j < num_of_params; j++) {
-                    if(indices_mode[j] == 0) {
-                        params[j] = elements[i+j+1];
-                    } else if(indices_mode[j] == 1) {
-                        params[j] = i+j+1;
-                    } else if(indices_mode[j] == 2) {
-                        params[j] = elements[i+j+1]+relative_base;
-                    }
-                }
-            
-                if(elements[params[0]] == 0) {
+                if(elements[param[0]] == 0) {
                     // because there is num_of_params+1 in for loop
                     num_of_params = -1;
-                    std::cout << "i je prije bio = " << i << ", a sad je = " << elements[params[1]] << std::endl;
-                    i = elements[params[1]];
+                    std::cout << "i je prije bio = " << i << ", a sad je = " << elements[param[1]] << std::endl;
+                    i = elements[param[1]];
                 }
             });
 
-            functions.emplace(7, [&]() {
+            functions.emplace(Operations::LESS_THAN, [&](std::array<long long int, 3> param) {
+                std::cout << "7" << std::endl;
                 num_of_params = 3;
-                std::array<long long int, 3> params;
-                for(int j = 0; j < num_of_params; j++) {
-                    if(indices_mode[j] == 0) {
-                        params[j] = elements[i+j+1];
-                    } else if(indices_mode[j] == 1) {
-                        params[j] = i+j+1;
-                    } else if(indices_mode[j] == 2) {
-                        params[j] = elements[i+j+1]+relative_base;
-                    }
-                }
-
-                if(elements[params[0]] < elements[params[1]]) {
-                    elements[params[2]] = 1;
+                if(elements[param[0]] < elements[param[1]]) {
+                    elements[param[2]] = 1;
                 } else {
-                    elements[params[2]] = 0;
+                    elements[param[2]] = 0;
                 }
             });
 
-            functions.emplace(8, [&]() {
+            functions.emplace(Operations::EQUALS, [&](std::array<long long int, 3> param) {
+                std::cout << "8" << std::endl;
                 num_of_params = 3;
-                std::array<long long int, 3> params;
-                for(int j = 0; j < num_of_params; j++) {
-                    if(indices_mode[j] == 0) {
-                        params[j] = elements[i+j+1];
-                    } else if(indices_mode[j] == 1) {
-                        params[j] = i+j+1;
-                    } else if(indices_mode[j] == 2) {
-                        params[j] = elements[i+j+1]+relative_base;
-                    }
-                }
-
-                if(elements[params[0]] == elements[params[1]]) {
-                    elements[params[2]] = 1;
+                if(elements[param[0]] == elements[param[1]]) {
+                    elements[param[2]] = 1;
                 } else {
-                    elements[params[2]] = 0;
+                    elements[param[2]] = 0;
                 }
             });
-        
-            functions.emplace(9, [&]() {
+       
+            functions.emplace(Operations::RELATIVE_BASE, [&](std::array<long long int, 3> param) {
+                std::cout << "9" << std::endl;
+                std::cout << "Sada je operacija 9 " << std::endl;
                 num_of_params = 1;
-                int el;
-                if(indices_mode[0] == 0) {
-                    el = elements[elements[i+1]];
-                } else if(indices_mode[0] == 1) {
-                    el = elements[i+1];
-                } else if(indices_mode[0] == 2) {
-                    el = elements[elements[i+1]+relative_base];
-                }
-            
-                relative_base += el;
+                std::cout << "params[0] = " << param[0] << std::endl;
+                relative_base += elements[param[0]];
             });
         }
 
         void calculate() {
-
-            for(i = 0; i < elements.size() && elements[i] != 99; i += num_of_params+1) {
+            std::array<long long int, 3> params;
+            for(i = 0; i < elements.size() && elements[i] != Operations::HALT; i += num_of_params+1) {
                 element = std::to_string(elements[i]);
                 std::cout << "element = " << element << std::endl;
                 int help = 0;
@@ -208,23 +141,56 @@ class Intcode_calculator {
                     }
                 }
                 std::cout << "naredba = " << element[element.size()-1]-ASCII_ZERO << std::endl;
-                functions[element[element.size()-1]-ASCII_ZERO]();
+               
+                for(int j = 0; j < 3; j++) {
+                    // last element and it is 99
+                    if(i+j+1 >= elements.size()) {
+                        params[j] = 0;
+                        continue;
+                    }
+                   
+                    if(indices_mode[j] == 0) {
+                        params[j] = elements[i+j+1];
+                    } else if(indices_mode[j] == 1) {
+                        params[j] = i+j+1;
+                    } else if(indices_mode[j] == 2) {
+                        params[j] = elements[i+j+1]+relative_base;
+                    }
+                   
+                    // if index greater than elements size
+                    if(params[j] > elements.size()) {
+                        fill_with_zeros(elements, params[j]);
+                    }
+                }
+                std::cout << "element = " << element << std::endl;
+                std::cout << "index = " << element[element.size()-1]-ASCII_ZERO << std::endl;
+                std::cout << "params[0] = " << params[0] << std::endl;
+                std::cout << "params[1] = " << params[1] << std::endl;
+                std::cout << "params[2] = " << params[2] << std::endl;
+               
+                std::cout << "func size = " << functions.size() << std::endl;
+                functions[element[element.size()-1]-ASCII_ZERO](params);
             }
         }
 
     private:
-        std::map<int, std::function<void()>> functions;
         int num_of_params = 1;
         int relative_base = 0;
-        std::vector<int> elements;
+        std::vector<long long int> elements;
         std::array<int, 3> indices_mode = {0};
         std::string element;
         long long int output;
         int i;
         int input;
+       
+        void fill_with_zeros(std::vector<long long int> &elements, long long int index) {
+            //std::for_each(elements.end(), elements.end() + index, [](long long int &num) {num = 0;});
+            int size = elements.size();
+            for(int i = size; i <= index; i++) {
+                elements[i] = 0;
+            }
+        }
 };
-
-void day5(std::vector<long long int>, int);
 
 int main() {
     int input;
@@ -242,185 +208,8 @@ int main() {
         elements.push_back(std::stoll(element));
     }
    
-    day5(elements, input);
+    Intcode_calculator calc(elements, input);
+    calc.calculate();
 
     return 0;
-}
-
-void day5(std::vector<long long int> elements, int input) {
-    // int num_of_params = 1;
-    // std::string element;
-    // long long int output;
-    // std::array<int, 3> indices_mode = {0};
-    // int i;
-    // int relative_base = 0;
-    // std::map<int, std::function<void()>> functions;
-   
-    // functions.emplace(1, [&]() {
-    //     num_of_params = 3;
-    //     std::array<long long int, 3> params;
-    //     for(int j = 0; j < num_of_params; j++) {
-    //         if(indices_mode[j] == 0) {
-    //             params[j] = elements[i+j+1];
-    //         } else if(indices_mode[j] == 1) {
-    //             params[j] = i+j+1;
-    //         } else if(indices_mode[j] == 2) {
-    //             params[j] = elements[i+j+1]+relative_base;
-    //         }
-    //     }
-    //     elements[params[2]] = elements[params[0]] + elements[params[1]];
-    // });
-
-    // functions.emplace(2, [&]() {
-    //     num_of_params = 3;
-    //     std::array<long long int, 3> params;
-    //     for(int j = 0; j < num_of_params; j++) {
-    //         if(indices_mode[j] == 0) {
-    //             params[j] = elements[i+j+1];
-    //         } else if(indices_mode[j] == 1) {
-    //             params[j] = i+j+1;
-    //         } else if(indices_mode[j] == 2) {
-    //             params[j] = elements[i+j+1]+relative_base;
-    //         }
-    //     }
-    //     elements[params[2]] = elements[params[0]] * elements[params[1]];    
-    // });
-
-    // functions.emplace(3, [&]() {
-    //     num_of_params = 1;
-    //     int ind;
-    //     if(indices_mode[2] == 0) {
-    //         ind = elements[i+1];
-    //     } else if(indices_mode[2] == 1) {
-    //         ind = i+1;
-    //     } else if(indices_mode[2] == 2) {
-    //         ind = elements[i+1]+relative_base;
-    //     }
-       
-    //     elements[ind] = input;
-    // });
-
-    // functions.emplace(4, [&]() {
-    //     num_of_params = 1;
-    //     if(indices_mode[0] == 0) {
-    //         output = elements[elements[i+1]];
-    //     } else if(indices_mode[0] == 1) {
-    //         output = elements[i+1];
-    //     } else if(indices_mode[0] == 2) {
-    //         if(elements[i+1]+relative_base >= elements.size()) {
-    //             fill_with_zeros();
-    //         }
-    //         output = elements[elements[i+1]+relative_base];
-    //     }
-    //     std::cout << "output = " << output << std::endl;
-    // });
-
-    // functions.emplace(5, [&]() {
-    //     num_of_params = 2;
-    //     std::array<long long int, 2> params;
-    //     for(int j = 0; j < num_of_params; j++) {
-    //         if(indices_mode[j] == 0) {
-    //             params[j] = elements[i+j+1];
-    //         } else if(indices_mode[j] == 1) {
-    //             params[j] = i+j+1;
-    //         } else if(indices_mode[j] == 2) {
-    //             params[j] = elements[i+j+1]+relative_base;
-    //         }
-    //     }
-    //     if(elements[params[0]] != 0) {
-    //         // because there is num_of_params+1 in for loop
-    //         num_of_params = -1;
-    //         i = elements[params[1]];
-    //     }
-    // });
-
-    // functions.emplace(6, [&]() {
-    //      num_of_params = 2;
-    //     std::array<long long int, 2> params;
-    //     for(int j = 0; j < num_of_params; j++) {
-    //         if(indices_mode[j] == 0) {
-    //             params[j] = elements[i+j+1];
-    //         } else if(indices_mode[j] == 1) {
-    //             params[j] = i+j+1;
-    //         } else if(indices_mode[j] == 2) {
-    //             params[j] = elements[i+j+1]+relative_base;
-    //         }
-    //     }
-       
-    //     if(elements[params[0]] == 0) {
-    //         // because there is num_of_params+1 in for loop
-    //         num_of_params = -1;
-    //         std::cout << "i je prije bio = " << i << ", a sad je = " << elements[params[1]] << std::endl;
-    //         i = elements[params[1]];
-    //     }
-    // });
-
-    // functions.emplace(7, [&]() {
-    //     num_of_params = 3;
-    //     std::array<long long int, 3> params;
-    //     for(int j = 0; j < num_of_params; j++) {
-    //         if(indices_mode[j] == 0) {
-    //             params[j] = elements[i+j+1];
-    //         } else if(indices_mode[j] == 1) {
-    //             params[j] = i+j+1;
-    //         } else if(indices_mode[j] == 2) {
-    //             params[j] = elements[i+j+1]+relative_base;
-    //         }
-    //     }
-
-    //     if(elements[params[0]] < elements[params[1]]) {
-    //         elements[params[2]] = 1;
-    //     } else {
-    //         elements[params[2]] = 0;
-    //     }
-    // });
-
-    // functions.emplace(8, [&]() {
-    //     num_of_params = 3;
-    //     std::array<long long int, 3> params;
-    //     for(int j = 0; j < num_of_params; j++) {
-    //         if(indices_mode[j] == 0) {
-    //             params[j] = elements[i+j+1];
-    //         } else if(indices_mode[j] == 1) {
-    //             params[j] = i+j+1;
-    //         } else if(indices_mode[j] == 2) {
-    //             params[j] = elements[i+j+1]+relative_base;
-    //         }
-    //     }
-
-    //     if(elements[params[0]] == elements[params[1]]) {
-    //         elements[params[2]] = 1;
-    //     } else {
-    //         elements[params[2]] = 0;
-    //     }
-    // });
-   
-    // functions.emplace(9, [&]() {
-    //     num_of_params = 1;
-    //     int el;
-    //     if(indices_mode[0] == 0) {
-    //         el = elements[elements[i+1]];
-    //     } else if(indices_mode[0] == 1) {
-    //         el = elements[i+1];
-    //     } else if(indices_mode[0] == 2) {
-    //         el = elements[elements[i+1]+relative_base];
-    //     }
-       
-    //     relative_base += el;
-    // });
-
-    // for(i = 0; i < elements.size() && elements[i] != 99; i += num_of_params+1) {
-    //     element = std::to_string(elements[i]);
-    //     std::cout << "element = " << element << std::endl;
-    //     int help = 0;
-    //     for(int k = 0; k < 3; k++) {
-    //         if(element.size() > 4-k) {
-    //             indices_mode[2-k] = element.at(help++)-ASCII_ZERO;
-    //         } else {
-    //             indices_mode[2-k] = 0;
-    //         }
-    //     }
-    //     std::cout << "naredba = " << element[element.size()-1]-ASCII_ZERO << std::endl;
-    //     functions[element[element.size()-1]-ASCII_ZERO]();
-    // }
 }
