e9604d278db99a69e7422208c97a1a452df4a9cb
Daria2002 <daria.matkovic@fer.hr>
2019-12-25 15:50:25 +0100

    Day12, part1 with printing

--- day12.cpp
+++ day12.cpp
@@ -3,12 +3,14 @@
 #include <algorithm>
 #include <fstream>
 #include <iostream> 
+#include <cmath>
 
-#define NUMBER_OF_STEPS 100
+#define NUMBER_OF_STEPS 1000
 
 class Moon {
 
     public:
+        int _x, _y, _z, _v_x, _v_y, _v_z;
         Moon(int x, int y, int z) : _x(x), _y(y), _z(z)
         {
             _v_x = _v_y = _v_z = 0;
@@ -17,21 +19,21 @@ class Moon {
         void update_velocities(std::vector<Moon> moon) {
             for(int i = 0; i < moon.size(); i++) {
                 if(moon[i]._x < _x) {
-                    _v_x++;
-                } else if(moon[i]._x > _x) {                    
                     _v_x--;
+                } else if(moon[i]._x > _x) {                    
+                    _v_x++;
                 }   
 
                 if(moon[i]._y < _y) {
-                    _v_y++;
-                } else if(moon[i]._y > _y) {
                     _v_y--;
+                } else if(moon[i]._y > _y) {
+                    _v_y++;
                 }
 
                 if(moon[i]._z < _z) {
-                    _v_z++;
-                } else if(moon[i]._z > _z) {
                     _v_z--;
+                } else if(moon[i]._z > _z) {
+                    _v_z++;
                 }
             }
         }
@@ -41,9 +43,6 @@ class Moon {
             _y = _y + _v_y;
             _z = _z + _v_z;
         }
-
-    private:
-        int _x, _y, _z, _v_x, _v_y, _v_z;
 };
 
 std::string get_str_between_two_str(const std::string s,
@@ -62,7 +61,7 @@ std::string get_str_between_two_str(const std::string s,
             last_delim_pos - end_pos_of_first_delim);
 }
 
-void get_initial_state(std::vector<Moon> moons) {
+void get_initial_state(std::vector<Moon>& moons) {
     std::fstream file("./day12.txt");
     std::string line;
 
@@ -82,30 +81,71 @@ void get_initial_state(std::vector<Moon> moons) {
     }
 }
 
+int calculate_total_energy(std::vector<Moon> state) {
+    std::array<int, 4> potential;
+    // potential
+    for(int i = 0; i < 4; i++) {
+        potential[i] = std::abs(state[i]._x) + 
+                       std::abs(state[i]._y) +
+                       std::abs(state[i]._z);
+        std::cout << "potential[" << i << "] = " << potential[i] << std::endl;
+    }
+
+    std::array<int, 4> kinetic;
+    // kinetic
+    for(int i = 0; i < 4; i++) {
+        kinetic[i] = std::abs(state[i]._v_x) + 
+                     std::abs(state[i]._v_y) +
+                     std::abs(state[i]._v_z);
+        std::cout << "kinetic[" << i << "] = " << kinetic[i] << std::endl;
+    }
+
+    std::array<int, 4> total;
+    for(int i = 0; i < 4; i++) {
+        total[i] = potential[i] * kinetic[i];
+        std::cout << "total[" << i << "] = " << total[i] << std::endl;
+    }
+
+    // return sum of total;
+    int sum_of_total = total[0] + total[1] + total[2] + total[3];
+    std::cout << "sum of total = " << sum_of_total << std::endl;
+    return sum_of_total;
+}
+
 int main() {
     // key is step, value is vector of four states (for each moon)
     std::map<int, std::vector<Moon>> moons;
     // get initial position, for step = 0
     get_initial_state(moons[0]);
 
-    std::vector<Moon> state;
+    std::vector<Moon> state = moons[0];
+    for(int i = 0; i < 4; i++)
+    std::printf("<x=%d, y=%d, z=%d>         <v_x=%d, v_y=%d, v_z=%d>\n",
+        state[i]._x, state[i]._y, state[i]._z, state[i]._v_x, state[i]._v_y, state[i]._v_z);
     // calculate for defined number of steps
-    for(int i = 1; i < NUMBER_OF_STEPS; i++) {
-        // for every moon update position and velocity
-        state = moons[i-1];
+    for(int i = 1; i < NUMBER_OF_STEPS+1; i++) {
         int j = 0;
-        std::for_each(state.begin(), state.end(), [&](Moon moon) {
+        state.clear();
+
+        std::cout << "korak = " << i << std::endl;
+
+        std::for_each(moons[i-1].begin(), moons[i-1].end(), [&](Moon moon) {
             // help contains all moons except moon to update
-            std::vector<Moon> help = state;
+            std::vector<Moon> help = moons[i-1];
             help.erase(help.begin() + j);
             j++;
 
             moon.update_velocities(help);
             moon.update_positions();
             state.push_back(moon);
+
+            std::printf("<x=%d, y=%d, z=%d>         <v_x=%d, v_y=%d, v_z=%d>\n",
+             moon._x, moon._y, moon._z, moon._v_x, moon._v_y, moon._v_z);
         });
+
         moons[i] = state;
-        state.empty();
     }
 
+    int part1 = calculate_total_energy(state);
+    std::cout << "part1 = " << part1 << std::endl;
 }
