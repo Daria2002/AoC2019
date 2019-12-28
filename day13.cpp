785aa47561c510039df3bdc9a85aa7990a751e94
Daria2002 <daria.matkovic@fer.hr>
2019-12-28 17:43:05 +0100

    Adapt intcode calculator, so now it returns only one output. Part 1 works. Add few things for part2.

--- day13.cpp
+++ day13.cpp
@@ -15,11 +15,18 @@
 
 #define ASCII_ZERO 48
 
+class Tile {
+    public:
+        int _x, _y, _z;
+        Tile(int x, int y, int z) : _x(x), _y(y), _z(z) { }
+};
+
 class Intcode_calculator {
 
     enum Operations {
         SUM = 1,
         MUL = 2,
+        SAVE_INPUT = 3,
         OUTPUT = 4,
         JUMP_IF_TRUE = 5,
         JUMP_IF_FALSE = 6,
@@ -36,7 +43,7 @@ class Intcode_calculator {
     };
    
     public:
-        std::map<int, std::function<void()>> functions;
+        std::map<int, std::function<int()>> functions;
 
         Intcode_calculator(std::vector<long long int> _elements) : elements(_elements)
         {
@@ -44,18 +51,27 @@ class Intcode_calculator {
             functions.emplace(Operations::SUM, [&]() {
                 num_of_params = 3;
                 elements[params[2]] = elements[params[0]] + elements[params[1]];
+                return 0;
             });
 
             functions.emplace(Operations::MUL, [&]() {
                 num_of_params = 3;
-                elements[params[2]] = elements[params[0]] * elements[params[1]];    
+                elements[params[2]] = elements[params[0]] * elements[params[1]];
+                return 0; 
+            });
+
+            functions.emplace(Operations::SAVE_INPUT, [&]() {
+                num_of_params = 1;
+                std::cin >> input;
+                elements[params[0]] = input;
+                return 0;
             });
 
             functions.emplace(Operations::OUTPUT, [&]() {
                 num_of_params = 1;
                 output = elements[params[0]];
-                // std::cout << "output = " << output << std::endl;
                 outputs.push_back(output);
+                return output;
             });
 
             functions.emplace(Operations::JUMP_IF_TRUE, [&]() {
@@ -65,6 +81,7 @@ class Intcode_calculator {
                     num_of_params = -1;
                     i = elements[params[1]];
                 }
+                return 0;
             });
 
             functions.emplace(Operations::JUMP_IF_FALSE, [&]() {
@@ -74,6 +91,7 @@ class Intcode_calculator {
                     num_of_params = -1;
                     i = elements[params[1]];
                 }
+                return 0;
             });
 
             functions.emplace(Operations::LESS_THAN, [&]() {
@@ -83,6 +101,7 @@ class Intcode_calculator {
                 } else {
                     elements[params[2]] = 0;
                 }
+                return 0;
             });
 
             functions.emplace(Operations::EQUALS, [&]() {
@@ -92,16 +111,19 @@ class Intcode_calculator {
                 } else {
                     elements[params[2]] = 0;
                 }
+                return 0;
             });
        
             functions.emplace(Operations::RELATIVE_BASE, [&]() {
                 num_of_params = 1;
                 relative_base += elements[params[0]];
+                return 0;
             });
         }
 
-        std::vector<int> calculate() {
-            for(i = 0; i < elements.size() && elements[i] != Operations::HALT; i += num_of_params+1) {
+        int calculate() {
+            for(i = last_index; i < elements.size() && elements[i] != Operations::HALT; i += num_of_params + 1) {
+                // std::cout << "i = " << i << std::endl;
                 element = std::to_string(elements[i]);
                 int help = 0;
                 for(int k = 0; k < 3; k++) {
@@ -121,11 +143,11 @@ class Intcode_calculator {
                     }
                    
                     if(indices_mode[j] == 0) {
-                        params[j] = elements[i+j+1];
+                        params[j] = elements[i + j + 1];
                     } else if(indices_mode[j] == 1) {
-                        params[j] = i+j+1;
+                        params[j] = i + j + 1;
                     } else if(indices_mode[j] == 2) {
-                        params[j] = elements[i+j+1]+relative_base;
+                        params[j] = elements[i + j + 1]+relative_base;
                     }
                    
                     // if index greater than elements size
@@ -133,13 +155,26 @@ class Intcode_calculator {
                         fill_with_zeros(elements, params[j]);
                     }
                 }
-                if(element[element.size()-1]-ASCII_ZERO == 3) {
-                    std::cout << "three" << std::endl;
+                if(element[element.size() - 1] - ASCII_ZERO == 4) {
+                    int result = functions[element[element.size()-1]-ASCII_ZERO]();
+                    last_index = i + num_of_params + 1;
+                    return result;
                 }
+
                 functions[element[element.size()-1]-ASCII_ZERO]();
             }
+            return -1;
+        }
 
-            return outputs;
+        int last_index = 0;
+        int get_number_of_block_tiles() {
+            int counter = 0;
+            for(int i = 2; i < outputs.size(); i = i + 3) {
+                if(outputs[i] == 2) {
+                    counter++;
+                }
+            }
+            return counter;
         }
 
     private:
@@ -162,9 +197,66 @@ class Intcode_calculator {
         }
 };
 
+void update_matrix(int x, int y, int tile_id, std::vector<std::vector<char>> matrix) {
+    char tile;
+    switch (tile_id)
+    {
+    case 0:
+        tile = '.';
+        break;
+    case 1:
+        tile = '|';
+        break;
+    case 2:
+        tile = '#';
+        break;
+    case 3:
+        tile = '_';
+        break;
+    case 4:
+        tile = 'o';
+        break;
+    default:
+        break;
+    }
+    
+    matrix[y][x] = tile;
+}
+
+void draw_matrix(std::vector<std::vector<char>> matrix) {
+    for(int i = 0; i < matrix.size(); i++) {
+        for(int j = 0; j < matrix[i].size(); j++) {
+            std::cout << matrix[i][j];
+        }
+        std::cout << " " << std::endl;
+    }
+}
+
+int max_dimension(std::vector<Tile> tiles, int dimension) {
+    // dimension == 0 -> width, dimension == 1 -> height
+    int width;
+    int height;
+    for(int i = 0; i < tiles.size(); i++) {
+        if(tiles[i]._x > width) {
+            width = tiles[i]._x;
+        }
+        if(tiles[i]._y > width) {
+            width = tiles[i]._y;
+        }
+    }
+
+    return (dimension == 0 ? width : height);
+}
+
+void initialize_matrix(int width, int height, std::vector<std::vector<char>>& matrix) {
+    matrix = std::vector<std::vector<char>> (
+        height, std::vector<char> (width, '.')
+    );
+}
+
 int main() {
     int counter = 0;
-   
+    bool matrix_initialized = false;
     std::ifstream file("./day13.txt");
     std::string line;
     getline(file, line);
@@ -178,23 +270,46 @@ int main() {
     }
    
     Intcode_calculator calc(elements);
+    std::vector<std::vector<char>> matrix;
 
-    std::vector<int> outputs = calc.calculate();
-
-    for(int i = 2; i < outputs.size(); i = i + 3) {
-        if(outputs[i] == 2) {
-            counter++;
+    while(calc.last_index < elements.size() && elements[calc.last_index] != 99) {
+        if(calc.calculate() == -1) {
+            break;
         }
     }
 
-    std::cout << "part1 = " << counter << std::endl;
+    std::cout << "part1 = " << calc.get_number_of_block_tiles() << std::endl;
 
     // part2
 
     // Memory address 0 represents the number of quarters
     // that have been inserted; set it to 2 to play for free.
     elements[0] = 2;
-    
+    Intcode_calculator calc2(elements);
+    std::vector<Tile> tiles;
+
+    while(calc2.last_index < elements.size() && elements[calc.last_index] != 99) {
+        int x = calc2.calculate();
+        int y = calc2.calculate();
+        int value = calc2.calculate();
+
+        if(x == -1 && y == 0) {
+            std::cout << "score = " << value << std::endl;
+            if(!matrix_initialized) {
+                int width = max_dimension(tiles, 0);
+                int height = max_dimension(tiles, 1);
+                initialize_matrix(width, height, matrix);
+            }
+            matrix_initialized = true;
+        } else {
+            if(matrix_initialized) {
+                update_matrix(x, y, value, matrix);
+                draw_matrix(matrix);
+            } else {
+                tiles.push_back(Tile(x, y, value));
+            }
+        }
+    }
     return 0;
 }
 
