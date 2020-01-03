b73c5ba42500c62d7a0166c06279462f1ce6076c
Daria2002 <daria.matkovic@fer.hr>
2020-01-03 20:34:50 +0100

    Remove help file and make costum unordered_map for costum class.

--- day14.cpp
+++ day14.cpp
@@ -26,43 +26,40 @@ class Chemical {
         std::string name;
         int quantity;
 
+        bool operator == (const Chemical &other) const {
+            return (name == other.name && quantity == other.quantity);
+        }
+
     private:
 };
 
+struct DataHasher
+{
+  std::size_t operator () (const Chemical &key) const 
+  {
+    std::size_t seed = 0;
+    boost::hash_combine(seed, boost::hash_value(key.name));
+    boost::hash_combine(seed, boost::hash_value(key.quantity));
+    return seed;
+  }
+};
+
 // key = inputs, value = outout
-std::unordered_map<std::string, std::vector<Chemical>> reactions;
-std::unordered_map<std::string, int> name_and_quantity;
-// fuel reaction
-std::vector<Chemical> fuel_reaction;
+/**
+ * To be able to use std::unordered_map (or one of the other unordered associative containers) with a user-defined key-type, you need to define two things:
+ * A Hash function -  this must be a class that overrides operator() and calculates the hash value given an object of the key-type. One particularly 
+ *                    straight-forward way of doing this is to specialize the std::hash template for your key-type.
+ * A comparison function for equality
+*/
+typedef std::unordered_map<Chemical, std::vector<Chemical>, DataHasher> DataHashMap;
+DataHashMap reactions;
+DataHashMap final_elements;
+std::vector<Chemical> fuel_inputs;
+typedef std::unordered_map<Chemical, int, DataHasher> OccurrencesMap;
+OccurrencesMap final_elements_occurrences_map;
 
-bool all_elements_ore(std::vector<Chemical>);
-int sum_ore(std::vector<Chemical>);
-int multiply_index(Chemical);
-void replace_reaction_with_inputs(Chemical, int, std::vector<Chemical> &);
-
-int calculate_ore(std::vector<Chemical> &inputs, int ore) {
-
-    if(all_elements_ore(inputs)) {
-        return sum_ore(inputs);
-    }
-
-    // for each input
-    for(int i = 0; i < inputs.size(); i++) {
-        // i-th input
-        int multiply_factor = multiply_index(inputs[i]);
-        replace_reaction_with_inputs(inputs[i], multiply_factor, inputs);
-
-        std::vector<Chemical> next_input;
-        next_input.push_back(inputs[i]);
-        int result = calculate_ore(next_input, ore);
-
-        ore += result;
-        // ore = calculate_ore(inputs);
-        inputs[i].name = ORE;
-        inputs[i].quantity = ore;
-    }
-
-    return ore;
+int count_occurrence(Chemical up, Chemical down, int counter) {
+    return 1;
 }
 
 int main() {
@@ -108,95 +105,25 @@ int main() {
         }
 
         std::vector<Chemical> inputs(inputs_and_outputs.begin(), inputs_and_outputs.begin() + inputs_and_outputs.size() - 1);
+        
         Chemical output = inputs_and_outputs[inputs_and_outputs.size()-1];
 
         if(fuel_elements) {
-            fuel_reaction = inputs;
+            fuel_inputs = inputs;
         } else {
-            reactions[output.name] = inputs;
-            name_and_quantity[output.name] = output.quantity;
+            if(inputs.size() == 1 && inputs[0].name == ORE) {
+                final_elements[output] = inputs;
+            } else {
+                reactions[output] = inputs;
+            }
         }
     }
     
-    int part1 = calculate_ore(fuel_reaction, 0);
-    std::cout << "part1 = " << part1 << std::endl;
+    for(std::unordered_map<Chemical, std::vector<Chemical>, DataHasher>::iterator iter = final_elements.begin(); iter != final_elements.end(); ++iter) {
+        for(int j = 0; j < fuel_inputs.size(); j++) {
+            final_elements_occurrences_map[iter -> first] += count_occurrence(fuel_inputs[j], iter->first, 0);
+        }
+    }
+    
     return 0;
 }
-
-// remove chemical and insert inputs to reaction where chemical is output
-void replace_reaction_with_inputs(Chemical chemical, int multiply_factor, std::vector<Chemical> &inputs) {
-    if(chemical.name == ORE || (reactions[chemical.name].size() == 1 && reactions[chemical.name].at(0).name == ORE)) {
-        return;
-    }
-
-    // insert inputs for chemical
-    for(auto it = reactions.begin(); it != reactions.end(); it++) {
-        if(it->first == chemical.name) {
-            // check inputs to reaction where chemical name is output
-            std::for_each(it -> second.begin(), it -> second.end(), [&](Chemical c) {
-                bool already_exists = false; 
-                for(int k = 0; k < inputs.size(); k++) {
-                    if(inputs[k].name == c.name) {
-                        already_exists = true;
-                        inputs[k].quantity += c.quantity*multiply_factor;
-                    }
-                }
-                if(!already_exists) {
-                    inputs.push_back(Chemical(c.name, c.quantity * multiply_factor));
-                }
-            });
-            break;
-        }
-    }
-
-    // remove chemical
-    int i;
-    for(i = 0; i < inputs.size(); i++) {
-        if(inputs[i].name == chemical.name) break;
-    }
-    inputs.erase(inputs.begin() + i);
-}
-
-bool all_elements_ore(std::vector<Chemical> inputs) {
-    for(int i = 0; i < inputs.size(); i++) {
-        if(inputs[i].name != ORE) return false;
-    }
-    return true;
-}
-
-int sum_ore(std::vector<Chemical> inputs) {
-    int count = 0;
-    std::for_each(inputs.begin(), inputs.end(), [&](Chemical chemical) {
-        count += chemical.quantity;
-    }); 
-
-    return count;
-}
-
-int multiply_index(Chemical chemical) {
-    int multiply_index = 0;
-    int new_quantity = chemical.quantity;
-    for(auto it : reactions) {
-        if(it.first == chemical.name) {
-            int old_quantity = name_and_quantity[it.first];
-            // check how many reactions need to happen to provide needed amoount of chemical
-            if(chemical.quantity / old_quantity == 0) {
-                multiply_index = 1;
-            } else if(chemical.quantity % old_quantity != 0) {
-                multiply_index = (chemical.quantity/old_quantity + 1);
-            } else {
-                multiply_index = chemical.quantity/old_quantity;
-            }
-            break;
-        }
-    }
-    return multiply_index;   
-}
-
-
-/**
- * pr. chemical quantity = 1, old_quantity = 3, new_quantity = 3
- * pr. chemical quantity = 4, old_quantity = 3, new_quantity = 2 * 4 = 8
- * 
- * 
- * */
