035bb4b91f48d481fc3567b0dcb2c5b81576f08b
Daria2002 <daria.matkovic@fer.hr>
2020-01-19 11:41:38 +0100

    Add input file and read file function in day16.cpp

--- day16.cpp
+++ day16.cpp
@@ -1,7 +1,12 @@
 #include <stdio.h>
 #include <ostream>
 #include <vector>
+#include <string>
+#include <string.h>
 #include <iostream>
+#include <istream>
+#include <fstream>
+#include <algorithm>
 
 template<class T>
 std::ostream& operator<<(std::ostream& stream, const std::pair<T, T>& pair) {
@@ -25,6 +30,24 @@ std::ostream& operator<<(std::ostream& stream, const std::vector<T>& values) {
   return stream;
 }
 
+std::vector<int> get_input_elements(std::string file_name) {
+    std::ifstream file(file_name);
+    std::string file_content;
+    getline(file, file_content);
+    std::vector<int> elements;
+
+    std::for_each(file_content.begin(), file_content.end(),
+     [&elements](const auto& c) {
+         elements.push_back(c-'0');
+    });
+
+    return elements;
+}
+
 int main() {
-    
+    std::vector<int> base_pattern = {0, 1, 0, -1};
+
+    std::vector<int> input_elements = get_input_elements("./day16.txt");
+
+    std::cout << input_elements;
 }
