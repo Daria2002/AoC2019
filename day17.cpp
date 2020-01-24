2a7364ab3aae13a6159c2ffe404afe4e14fb45f7
Daria2002 <daria.matkovic@fer.hr>
2020-01-24 23:00:52 +0100

    Add intcode calculator.

--- day17.cpp
+++ day17.cpp
@@ -2,6 +2,10 @@
 #include <vector>
 #include <ostream>
 #include <iostream>
+#include <map>
+#include <functional>
+
+#define ASCII_ZERO 48
 
 template<class T>
 std::ostream& operator<<(std::ostream& stream, const std::pair<T, T>& pair) {
@@ -19,6 +23,158 @@ std::ostream& operator<<(std::ostream& stream, const std::vector<T>& v) {
     stream << "]";
     return stream;
 } 
+
+class Intcode_calculator {
+
+    enum Operations {
+        SUM = 1,
+        MUL = 2,
+        SAVE_INPUT = 3,
+        OUTPUT = 4,
+        JUMP_IF_TRUE = 5,
+        JUMP_IF_FALSE = 6,
+        LESS_THAN = 7,
+        EQUALS = 8,
+        RELATIVE_BASE = 9,
+        HALT = 99
+    };
+
+    enum Modes {
+        POSITION = 0,
+        IMMIDIATE  = 1,  
+        RELATIVE = 2
+    };
+   
+    public:
+        std::map<int, std::function<void()>> functions;
+        Intcode_calculator(std::vector<long long int> _elements) : elements(_elements)
+        {
+            last_index = 0;
+            // capture class members by saying 'this' in the capture list
+            functions.emplace(Operations::SUM, [&]() {
+                num_of_params = 3;
+                elements[params[2]] = elements[params[0]] + elements[params[1]];
+            });
+
+            functions.emplace(Operations::MUL, [&]() {
+                num_of_params = 3;
+                elements[params[2]] = elements[params[0]] * elements[params[1]];    
+            });
+
+            functions.emplace(Operations::SAVE_INPUT, [&]() {
+                num_of_params = 1;
+                elements[params[0]] = input;
+            });
+
+            functions.emplace(Operations::OUTPUT, [&]() {
+                num_of_params = 1;
+                output = elements[params[0]];
+            });
+
+            functions.emplace(Operations::JUMP_IF_TRUE, [&]() {
+                num_of_params = 2;
+                if(elements[params[0]] != 0) {
+                    // because there is num_of_params+1 in for loop
+                    num_of_params = -1;
+                    i = elements[params[1]];
+                }
+            });
+
+            functions.emplace(Operations::JUMP_IF_FALSE, [&]() {
+                num_of_params = 2;
+                if(elements[params[0]] == 0) {
+                    // because there is num_of_params+1 in for loop
+                    num_of_params = -1;
+                    i = elements[params[1]];
+                }
+            });
+
+            functions.emplace(Operations::LESS_THAN, [&]() {
+                num_of_params = 3;
+                if(elements[params[0]] < elements[params[1]]) {
+                    elements[params[2]] = 1;
+                } else {
+                    elements[params[2]] = 0;
+                }
+            });
+
+            functions.emplace(Operations::EQUALS, [&]() {
+                num_of_params = 3;
+                if(elements[params[0]] == elements[params[1]]) {
+                    elements[params[2]] = 1;
+                } else {
+                    elements[params[2]] = 0;
+                }
+            });
+       
+            functions.emplace(Operations::RELATIVE_BASE, [&]() {
+                num_of_params = 1;
+                relative_base += elements[params[0]];
+            });
+        }
+
+        long long int calculate(int _input) {
+            input = _input;
+            for(i = last_index; i < elements.size() && elements[i] != Operations::HALT; i += num_of_params+1) {
+                element = std::to_string(elements[i]);
+                int help = 0;
+                for(int k = 0; k < 3; k++) {
+                    if(element.size() > 4-k) {
+                        indices_mode[2-k] = element.at(help++)-ASCII_ZERO;
+                    } else {
+                        indices_mode[2-k] = 0;
+                    }
+                }
+
+                for(int j = 0; j < 3; j++) {
+                    // last element and it is 99
+                    if(i+j+1 >= elements.size()) {
+                        params[j] = 0;
+                        continue;
+                    }
+                   
+                    if(indices_mode[j] == 0) {
+                        params[j] = elements[i+j+1];
+                    } else if(indices_mode[j] == 1) {
+                        params[j] = i+j+1;
+                    } else if(indices_mode[j] == 2) {
+                        params[j] = elements[i+j+1]+relative_base;
+                    }
+                   
+                    // if index greater than elements size
+                    if(params[j] > elements.size()) {
+                        fill_with_zeros(elements, params[j]);
+                    }
+                }
+
+                functions[element[element.size()-1]-ASCII_ZERO]();
+                if(element[element.size()-1]-ASCII_ZERO == 4) {
+                    last_index = i + num_of_params + 1;
+                    return output;
+                }
+            }
+            return -1;
+        }
+
+    private:
+        int last_index;
+        std::vector<long long int> elements;
+        std::array<long long int, 3> params;
+        int num_of_params = 1;
+        int relative_base = 0;
+        std::array<int, 3> indices_mode = {0};
+        std::string element;
+        long long int output;
+        int i;
+        int input;
+       
+        void fill_with_zeros(std::vector<long long int> &elements, long long int index) {
+            int size = elements.size();
+            for(int i = size; i <= index; i++) {
+                elements.push_back(0);
+            }
+        }
+};
 
 int main() {
     
