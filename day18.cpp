4e95eeb07e33dc699ffce551d0cd2bb0f16fa697
Daria2002 <daria.matkovic@fer.hr>
2020-02-20 22:05:15 +0100

    Add few things

--- day18.cpp
+++ day18.cpp
@@ -1,28 +1,82 @@
 #include <stdio.h>
 #include <iostream>
 #include <vector>
+#include <unordered_set>
+
+class Position {
+    public:
+        int x, y; // 0 is first element inside od #
+};
 
 class Base {
-    // position
-    // add function for calculating distance from @ 
+    public:
+        Base() {}
+        std::string name;
+        Position position;
+};
+
+bool operator==(const Base& x, const Base& y)
+{
+    return x.name == y.name;
+}
+
+struct hashBase
+{
+    std::string operator()(const Base& base) const {
+        return base.name;
+    }
 };
 
 class Door : public Base {
-
+    public:
+        Door() {}
+        inline void unlock() {
+            locked = false;
+        }
+        inline bool is_locked() const {
+            return locked;
+        }
+    private:
+        bool locked = true;
 };
 
 class Key : public Base {
-
+    public: 
+        Key() {}
 };
 
 class Field {
     public:
-        std::vector<Door> doors;
-        std::vector<Key> keys;
+        inline void add_door(Door door) {
+            doors.insert(door);
+        }
+
+        inline void add_key(Key key) {
+            keys.insert(key);
+        }
+
+        bool more_options(Position enterence_position) {
+
+        }
+
+        Position get_enterence_position() {
+
+        }
+
+        void set_enterence_position(Position position) {
+            enterence_position = position;
+        }
+
+        std::unordered_set<Door, hashBase> doors;
+        std::unordered_set<Key, hashBase> keys;
+
+    private:   
+        Position enterence_position;
 };
 
-bool door_explored(std::vector<Door> doors) {
-    return doors.size() > 0;
+template <typename T>
+inline bool is_explored(std::unordered_set<T, hashBase> elements) {
+    return elements.size() > 0;
 }
 
 Field get_field() {
@@ -34,11 +88,15 @@ Field get_field() {
 
 int get_shortest_path() {
     int counter = 0;
-
     Field field = get_field();
+    Position enterence_position = field.get_enterence_position();
 
-    while(!door_explored(field.doors)) {
-        
+    while(is_explored(field.doors) || is_explored(field.keys)) {
+        if(field.more_options(enterence_position)) {
+            // split in more cases
+        } else {
+            // go further, don't split
+        }
     }
 
     return counter;
